4103a57f418dc724757847b2ec39244822927a4f
Ben Fortier <b.fortier@fetchrewards.com>
2025-05-17 23:23:26 -0400

    Update BaseURL.swift

--- ResyPro/Packages/Network/Sources/Network/Endpoint/BaseURL.swift
+++ ResyPro/Packages/Network/Sources/Network/Endpoint/BaseURL.swift
@@ -8,5 +8,5 @@
 /// Base URLs used to construct requests.
 public enum BaseURL: String, Sendable {
   case resy = "https://api.resy.com"
-  case backend = "https://backend.example.com"
+  case backend = "https://resy-service.onrender.com"
 }
