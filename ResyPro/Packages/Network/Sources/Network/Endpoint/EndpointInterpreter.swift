3225e259980de566f99ef16f904bde3efc83f616
Ben Fortier <31415211+befortier@users.noreply.github.com>
2025-05-18 11:14:43 -0400

    Handle POST bodies in EndpointInterpreter

--- ResyPro/Packages/Network/Sources/Network/Endpoint/EndpointInterpreter.swift
+++ ResyPro/Packages/Network/Sources/Network/Endpoint/EndpointInterpreter.swift
@@ -23,6 +23,16 @@ public struct EndpointInterpreter {
     guard let finalURL = components?.url else { return nil }
     var request = URLRequest(url: finalURL)
     request.allHTTPHeaderFields = endpoint.headers
+
+    if let post = endpoint as? any PostEndpoint {
+      request.httpMethod = InterpretedHTTPMethod.post.rawValue
+      if let body = post.requestBody {
+        request.httpBody = try? JSONEncoder().encode(body)
+      }
+    } else if endpoint is GetEndpoint {
+      request.httpMethod = InterpretedHTTPMethod.get.rawValue
+    }
+
     return request
   }
 }
