9f9a6ec0073b06f444f051287c3f6cca460765b3
Ben Fortier <b.fortier@fetchrewards.com>
2025-05-18 21:32:27 -0400

    Update NotificationSubmissionResponse.swift

--- ResyPro/Packages/Notifications/Sources/Notifications/CreateBulkNotification/Data/Network/NotificationSubmissionResponse.swift
+++ ResyPro/Packages/Notifications/Sources/Notifications/CreateBulkNotification/Data/Network/NotificationSubmissionResponse.swift
@@ -22,63 +22,9 @@ public struct NotificationSubmissionResponse: Hashable, Sendable, Decodable {
         public let venueID: Venue.ID
 
         enum CodingKeys: String, CodingKey {
-            case venueID = "venue_id"
-            case day
-            case timePreferredStart = "time_preferred_start"
-            case timePreferredEnd = "time_preferred_end"
+            case interval
             case partySize = "num_seats"
-        }
-
-        public init(from decoder: Decoder) throws {
-            let container = try decoder.container(keyedBy: CodingKeys.self)
-
-            let day = try container.decode(String.self, forKey: .day)
-            let startTime = try container.decode(String.self, forKey: .timePreferredStart)
-            let endTime = try container.decode(String.self, forKey: .timePreferredEnd)
-
-            let venueID = try container.decode(Int.self, forKey: .venueID)
-            let partySize = try container.decode(Int.self, forKey: .partySize)
-
-            let formatter = DateFormatter()
-            formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-            formatter.timeZone = TimeZone(secondsFromGMT: 0)
-
-            guard let start = formatter.date(from: "\(day) \(startTime)"),
-                  let end = formatter.date(from: "\(day) \(endTime)")
-            else {
-                throw DecodingError.dataCorruptedError(
-                    forKey: .timePreferredStart,
-                    in: container,
-                    debugDescription: "Failed to parse start or end time into Date"
-                )
-            }
-
-            self.interval = DateInterval(start: start, end: end)
-            self.venueID = venueID
-            self.partySize = partySize
-        }
-
-        public func encode(to encoder: Encoder) throws {
-            var container = encoder.container(keyedBy: CodingKeys.self)
-
-            try container.encode(venueID, forKey: .venueID)
-            try container.encode(partySize, forKey: .partySize)
-
-            let formatter = DateFormatter()
-            formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-            formatter.timeZone = TimeZone(secondsFromGMT: 0)
-
-            let dayFormatter = DateFormatter()
-            dayFormatter.dateFormat = "yyyy-MM-dd"
-            dayFormatter.timeZone = TimeZone(secondsFromGMT: 0)
-
-            let dayString = dayFormatter.string(from: interval.start)
-            let startTime = formatter.string(from: interval.start).components(separatedBy: " ").last ?? ""
-            let endTime = formatter.string(from: interval.end).components(separatedBy: " ").last ?? ""
-
-            try container.encode(dayString, forKey: .day)
-            try container.encode(startTime, forKey: .timePreferredStart)
-            try container.encode(endTime, forKey: .timePreferredEnd)
+            case venueID = "venue_id"
         }
     }
 }
