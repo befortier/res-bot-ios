a4c1baf52844e9441b4f8c9133f974b263dcc5d8
Ben Fortier <31415211+befortier@users.noreply.github.com>
2025-05-31 11:55:18 -0400

    Use NotificationCard in date view

--- ResyPro/Packages/Notifications/Sources/Notifications/Notification/NotificationCalendarView.swift
+++ ResyPro/Packages/Notifications/Sources/Notifications/Notification/NotificationCalendarView.swift
@@ -32,43 +32,19 @@ struct NotificationCalendarView: View {
 
             ScrollView {
                 if let tickets = notificationsByDay[Calendar.current.startOfDay(for: selectedDate)] {
-                    let grouped = Dictionary(grouping: tickets, by: \ .venueID)
-                    let venues = grouped.compactMap { venuesByID[$0.key] }
-                    VerticalVenueCardGridView(venues: venues) { state in
-                        if let venueTickets = grouped[state.id] {
-                            VStack(spacing: 4) {
-                                VerticalVenueCard(viewState: state)
-                                    .frame(height: 200)
-                                SummaryLabel(tickets: venueTickets)
-                            }
+                    LazyVStack(spacing: 8) {
+                        ForEach(tickets, id: \ .self) { ticket in
+                            NotificationCard(
+                                viewState: .init(
+                                    request: ticket,
+                                    venue: venuesByID[ticket.venueID]
+                                )
+                            )
                         }
                     }
+                    .padding(.vertical, 8)
                 }
             }
         }
-    }
-}
-
-
-
-/// Displays a summary of party sizes and time for a set of tickets.
-struct SummaryLabel: View {
-    let tickets: [NotificationTicket]
-
-    private static let timeFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.timeStyle = .short
-        formatter.dateStyle = .none
-        return formatter
-    }()
-
-    var body: some View {
-        let sizes = Set(tickets.map(\.partySize)).sorted()
-        let sizeText = sizes.map { "\($0)p" }.joined(separator: ",")
-        let time = tickets.sorted { $0.interval.start < $1.interval.start }.first?.interval.start
-        let timeText = time.map { SummaryLabel.timeFormatter.string(from: $0) } ?? ""
-        Text("\(sizeText) • \(timeText)")
-            .font(.design(.caption2))
-            .foregroundStyle(Color.textSecondary)
     }
 }
